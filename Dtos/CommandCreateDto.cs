7418c96ee6291bb8932d5ad8bbb6f1595503b604
natandias <49736127+natandias@users.noreply.github.com>
2021-02-07 15:12:05 -0300

    Feat: add validation on command create dto

--- Dtos/CommandCreateDto.cs
+++ Dtos/CommandCreateDto.cs
@@ -1,13 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Commander.Dtos
 {
   public class CommandCreateDto
   {
     // public int Id { get; set; }
 
+    [Required]
+    [MaxLength(250)]
     public string HowTo { get; set; }
 
+    [Required]
     public string Line { get; set; }
 
+    [Required]
     public string Platform { get; set; }
   }
 }
